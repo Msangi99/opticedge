0c31d7ed13fa82ed6d4252aea7e115a6650b8e97
msangi2002-byte <msangi2002@gmail.com>
2026-02-22 14:51:16 +0300

    ooo

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,11 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
         Route::get('settings', [App\Http\Controllers\Admin\SettingController::class , 'index'])->name('settings.index');
         Route::post('settings', [App\Http\Controllers\Admin\SettingController::class , 'update'])->name('settings.update');
 
+        // Run whitelisted artisan command: GET /admin/command/{command}
+        Route::get('command/{command}', App\Http\Controllers\Admin\ArtisanCommandController::class)
+            ->where('command', '[a-zA-Z0-9:_-]+')
+            ->name('command.run');
+
         // Reports
         Route::get('reports', [App\Http\Controllers\Admin\ReportController::class , 'index'])->name('reports.index');
 
